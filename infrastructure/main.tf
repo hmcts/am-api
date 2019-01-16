2b4760a21dbf19634294c5cad721828c525097af
Remigiusz Kruk <R.Kruk@kainos.com>
2019-01-16 11:45:15 +0100

    add missing terraform code

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -2,6 +2,8 @@ provider "azurerm" {}
 
 locals {
   ase_name               = "${data.terraform_remote_state.core_apps_compute.ase_name[0]}"
+  shortEnv = "${(var.env == "preview" || var.env == "spreview") ? var.deployment_namespace : var.env}"
+
 }
 
 module "am-api" {
@@ -20,6 +22,60 @@ module "am-api" {
     LOGBACK_REQUIRE_ERROR_CODE  = "false"
   }
 }
+
+module "postgres-am-api" {
+  source              = "git@github.com:hmcts/moj-module-postgres?ref=master"
+  product             = "${var.product}-${var.component}"
+  env                 = "${var.env}"
+  location            = "${var.location_app}"
+  postgresql_user     = "${var.db_user}"
+  database_name       = "${var.db_name}"
+  postgresql_version  = "10"
+  common_tags         = "${var.common_tags}"
+}
+
+# region save DB details to Azure Key Vault
+module "am-vault-api" {
+  source = "git@github.com:hmcts/moj-module-key-vault?ref=master"
+  name = "${var.raw_product}-${var.component}-${local.shortEnv}"
+  product = "${var.product}-${var.component}"
+  env = "${var.env}"
+  tenant_id = "${var.tenant_id}"
+  object_id = "${var.jenkins_AAD_objectId}"
+  resource_group_name = "${azurerm_resource_group.rg.name}"
+  product_group_object_id = "${var.product_group_object_id}"
+}
+
+resource "azurerm_key_vault_secret" "POSTGRES-USER" {
+  name      = "${var.product}-${var.component}-POSTGRES-USER"
+  value     = "${module.postgres-am-api.user_name}"
+  vault_uri = "${module.am-vault-api.key_vault_uri}"
+}
+
+resource "azurerm_key_vault_secret" "POSTGRES-PASS" {
+  name      = "${var.product}-${var.component}-POSTGRES-PASS"
+  value     = "${module.postgres-am-api.postgresql_password}"
+  vault_uri = "${module.am-vault-api.key_vault_uri}"
+}
+
+resource "azurerm_key_vault_secret" "POSTGRES_HOST" {
+  name      = "${var.product}-${var.component}-POSTGRES-HOST"
+  value     = "${module.postgres-am-api.host_name}"
+  vault_uri = "${module.am-vault-api.key_vault_uri}"
+}
+
+resource "azurerm_key_vault_secret" "POSTGRES_PORT" {
+  name      = "${var.product}-${var.component}-POSTGRES-PORT"
+  value     = "${module.postgres-am-api.postgresql_listen_port}"
+  vault_uri = "${module.am-vault-api.key_vault_uri}"
+}
+
+resource "azurerm_key_vault_secret" "POSTGRES_DATABASE" {
+  name      = "${var.product}-${var.component}-POSTGRES-DATABASE"
+  value     = "${module.postgres-am-api.postgresql_database}"
+  vault_uri = "${module.am-vault-api.key_vault_uri}"
+}
+# endregion
 
 resource "azurerm_resource_group" "rg" {
   name     = "${var.product}-${var.env}"
--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -26,3 +26,39 @@ variable "capacity" {
 variable "common_tags" {
   type = "map"
 }
+
+variable "db_user" {
+  type        = "string"
+  default     = "snlnotes"
+  description = "Database user name"
+}
+
+variable "db_name" {
+  type        = "string"
+  default     = "snlnotesdb"
+  description = "Database name"
+}
+
+variable "raw_product" {
+  default     = "am" // jenkins-library overrides product for PRs and adds e.g. pr-118-ccd
+}
+
+variable "tenant_id" {
+  type                  = "string"
+  description           = "(Required) The Azure Active Directory tenant ID that should be used for authenticating requests to the key vault. This is usually sourced from environemnt variables and not normally required to be specified."
+}
+
+variable "jenkins_AAD_objectId" {
+  type                        = "string"
+  description                 = "(Required) The Azure AD object ID of a user, service principal or security group in the Azure Active Directory tenant for the vault. The object ID must be unique for the list of access policies."
+}
+
+variable "product_group_object_id" {
+  default = "c9ab670f-8f92-4170-ba08-796ccab27751"
+  description = "dcd_snl_kv"
+}
+
+variable "deployment_namespace" {
+  type        = "string"
+  default     = ""
+}
