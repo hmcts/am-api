493855dd4a558be6ff8bf959489beb5470491862
Leszek Gurniewicz <l.gurniewicz@kainos.com>
2019-01-21 14:13:56 +0100

    AM-50: key vault dynamic name test

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -104,7 +104,7 @@ resource "azurerm_resource_group" "rg" {
 }
 
 # region shared Azure Key Vault
-data "azurerm_key_vault" "am-api-vault" {
+data "azurerm_key_vault" "${var.product}-${var.component}-${var.env}" {
   name = "${local.vaultName}"
   resource_group_name = "${azurerm_resource_group.rg.name}"
 }
