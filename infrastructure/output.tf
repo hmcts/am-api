3968d8722520506f9b5f1445dcb669fc75a44538
Remigiusz Kruk <R.Kruk@kainos.com>
2019-01-21 15:18:17 +0100

    add sharedResourceGroup as an output

--- infrastructure/output.tf
+++ infrastructure/output.tf
@@ -6,6 +6,10 @@ output "vaultName" {
   value = "${module.am-vault-api.key_vault_name}"
 }
 
+output "sharedResourceGroup" {
+  value = "${local.sharedResourceGroup}"
+}
+
 output "shared_vault_uri" {
   value = "${data.azurerm_key_vault.am-shared-vault.vault_uri}"
 }
